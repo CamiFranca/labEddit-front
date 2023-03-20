68ad3fcc34ebf9562a93f42361894907fce3e2e6
CamiFranca <camillaartc@gmail.com>
2023-03-20 02:49:37 -0300

    pagina de login finalizada, post continua.

---
+++ projeto-labeddit/src/components/textBox/index.js
@@ -0,0 +1,11 @@
+export const TextBox = () => {
+
+    return (
+        <textarea name='postContent'
+            minlength="50" maxlength="255"
+        >
+        </textarea>
+    )
+}
+
+export default TextBox
